4bd045510f32beb2e40850d46ffc107d1b655f89
rwba7567 <rwba7567@gmail.com>
2023-03-12 19:37:26 +0800

    Prototype 6

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const endpoint = 'https://rwba7567.github.io/EID/';
 
 const updateButtonStatus = (status) => {
-  const button = document.querySelector('#button');
+  const button = document.querySelector('#table3-1');
   button.style.backgroundColor = status === 'On' ? 'green' : 'red';
 };
 

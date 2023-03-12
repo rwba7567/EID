2c0869ce46149c773cdedd1a734d78cc61f3c788
rwba7567 <rwba7567@gmail.com>
2023-03-12 19:24:55 +0800

    Prototype 4

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-fetch('https://rwba7567.github.io/EID/')
-  .then(response => response.json())
+const endpoint = 'https://rwba7567.github.io/EID/';
+
+const updateButtonStatus = (status) => {
+  const button = document.querySelector('#table1-3');
+  button.style.backgroundColor = status === 'On' ? 'green' : 'red';
+};
+
+fetch(endpoint)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
   .then(data => {
-    console.log(data);
-    // Use data to update your website here
+    updateButtonStatus(data.status);
   })
   .catch(error => {
-    console.error('Error:', error);
+    console.error('There was a problem with the fetch operation:', error);
   });

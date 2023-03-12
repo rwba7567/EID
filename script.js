748efd7e882159d45a8db10e240dbffd3932f59c
rwba7567 <rwba7567@gmail.com>
2023-03-12 19:00:55 +0800

    Prototype 1

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
+const http = require('http');
+
+// initialize button state to 'off'
+let buttonState = 'off';
+
+// create HTTP server to listen for incoming requests
 const server = http.createServer((req, res) => {
+  // check if incoming request is a POST request
   if (req.method === 'POST') {
     let body = '';
-    req.on('data', chunk => {
-      body += chunk.toString();
+    // listen for incoming data
+    req.on('data', (data) => {
+      body += data;
     });
+    // when all data is received, parse it and update button state
     req.on('end', () => {
-      console.log(`Received POST request with body: ${body}`);
-      res.end('Success');
+      const postData = JSON.parse(body);
+      buttonState = postData.buttonState;
+      console.log(`Button state is now ${buttonState}`);
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end(`Button state is now ${buttonState}`);
     });
   } else {
-    res.end('Invalid request method');
+    // return current button state for GET requests
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(`Button state is currently ${buttonState}`);
   }
 });
 
-server.listen(3000, () => {
-  console.log('Server listening on port 3000');
+// start HTTP server
+server.listen(8080, () => {
+  console.log('HTTP server listening on port 8080');
 });

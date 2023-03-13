4638aa2b13bec181abe5a2652afc63c7b2efc41e
rwba7567 <rwba7567@gmail.com>
2023-03-13 16:06:23 +0800

    final

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-const url = "172.20.10.6";
-
-function checkButtonStatus() {
-  fetch(url)
-    .then((response) => response.text())
-    .then((data) => {
-      console.log(data);
-      setTimeout(checkButtonStatus, 1000); // check again in 1 second
-    })
-    .catch((error) => {
-      console.log(error);
-      setTimeout(checkButtonStatus, 1000); // check again in 1 second
-    });
+function makeRequest() {
+  fetch('https://retoolapi.dev/b6Dto8/occupancy_tracker')
+    .then(response => response.json())
+    .then(data => update(data))
+    .catch(error => console.error(error));
 }
 
-checkButtonStatus();
+function update(responseData) {
+  console.log(responseData);
+
+  const obj = responseData.find(item => item.id === 1);
+  const occupancy = obj.occupancy;
+  console.log(occupancy)
+  
+
+  const button = document.querySelector('#table3-1');
+  button.style.backgroundColor = occupancy === false ? 'green' : 'red';
+  console.log("testing")
+}
+
+setInterval(makeRequest, 5000); // 5000 milliseconds = 5 seconds
+
